5eeb91c1d4da382c58b47482d69418a7eb341517
Andrea Raaschou <andrea.raaschou@gmail.com>
2026-02-25 15:41:27 +0100

    make partial derivative functions

--- task3.py
+++ task3.py
@@ -17,11 +17,34 @@ def func(z):
     dzdy = - 2 / (1 + 2*np.e**(2*z))
     return [dzdx, dzdy]
 
+def numerical_partial_wrt_x(f, h=10**(-7)):
+    '''
+    takes in function and precision and returns function of partial derivative with respect to x 
+    '''
+    def return_function(x, y, z):
+        return((f(x+h, y, z) - f(x,y, z))/h)
+    return(return_function)
 
+
+def numerical_partial_wrt_y(f, h=10**(-7)):
+    '''
+    takes in function and precision and returns function of partial derivative with respect to y 
+    '''
+    def return_function(x, y, z):
+        return((f(x, y+h, z) - f(x,y, z))/h)
+    return(return_function)
+
+def numerical_partial_wrt_z(f, h=10**(-7)):
+    '''
+    takes in function and precision and returns function of partial derivative with respect to z 
+    '''
+    def return_function(x, y, z):
+        return((f(x, y, z+h) - f(x,y, z))/h)
+    return(return_function)
 
 #root = fsolve(func, 0)
-x = np.linspace(-1, 1, 10)
-y = np.linspace(-1, 1, 10)
+x = np.linspace(-1, 1, 100)
+y = np.linspace(-1, 1, 100)
 X, Y = np.meshgrid(x, y)
 
 
@@ -30,10 +53,10 @@ for x_i in x:
     for y_i in y:
         root.append(fsolve(f, 0, args = (x_i,y_i)))
 root = np.array(root)
-root = np.reshape(root, (10, 10))
+root = np.reshape(root, (100, 100))
 
 
-# surface plot
+# surface plot for z(x,y)
 fig = plt.figure(figsize=(12, 5))
 ax1 = fig.add_subplot(121, projection='3d')
 ax1.plot_surface(X, Y, root)
