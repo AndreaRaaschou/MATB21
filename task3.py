20a8e4a05ebec897138eae5af833dec99e70c4cf
Andrea Raaschou <andrea.raaschou@gmail.com>
2026-02-25 16:12:36 +0100

    make plotting smooth

--- task3.py
+++ task3.py
@@ -12,12 +12,14 @@ from scipy.optimize import fsolve
 def f(x, y, z):
     return x + 2*y + z + np.e**(2*z) - 1
 
+"""
 def taylor_polynomial(z, point=(0, 0) , degree=2):
     def polynomial_return(x, y):
         for i in range(0, degree+1):
             for j in range(0, degree+1-i):
                 eee +=1
     return(polynomial_return)
+"""
 
 def numerical_partial_wrt_x(f, h=10**(-7)):
     '''
@@ -48,21 +50,28 @@ def plot_3D_surface(x, y, z, title):
     X, Y = np.meshgrid(x, y)
     fig = plt.figure(figsize=(12, 5))
     ax1 = fig.add_subplot(121, projection='3d')
-    ax1.plot_surface(X, Y, root)
+    ax1.plot_surface(X, Y, z)
     ax1.set_title(title)
     ax1.set_xlabel('X')
     ax1.set_ylabel('Y')
     ax1.set_zlabel('Z')
     fig.show()
     
-   
-# approximate z
-x = np.linspace(-1, 1, 100)
-y = np.linspace(-1, 1, 100)
+def calculate_z(x, y):
+    return fsolve(f, 0, args = (x, y))
 
-root = []
-for x_i in x:
-    for y_i in y:
-        root.append(fsolve(f, 0, args = (x_i,y_i)))
-root = np.reshape(np.array(root), (100, 100))
-plot_3D_surface(x, y, root, 'Z surface plot')
+def evaluate_z():
+    x = np.linspace(-1, 1, 100)
+    y = np.linspace(-1, 1, 100)
+    
+    root = []
+    for x_i in x:
+        for y_i in y:
+            root.append(calculate_z(x_i,y_i))
+    root = np.reshape(np.array(root), (100, 100))
+    return (x, y, root)
+
+
+plot_3D_surface(*evaluate_z(), 'Z surface plot')
+
+
